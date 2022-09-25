117d6f43da8353ef22be5de2599391c5de64321a
AmerBiro <amer.biro@gmail.com>
2022-09-25 11:16:45 +0100

    Google maps integrate, minSDK = 20

---
+++ lib/screens/simple_map.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SimpleMap extends StatefulWidget {
+  const SimpleMap({Key? key}) : super(key: key);
+
+  @override
+  State<SimpleMap> createState() => _SimpleMapState();
+}
+
+class _SimpleMapState extends State<SimpleMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
